e9582f464daa938f4f36d761429a80f79400655e
Karsten Knese <Karsten1987@users.noreply.github.com>
2020-01-17 05:18:32 -0800

    migrate ros2 devel (#11)
    * Initial port to ros 2
    
    Initial port of the urg_c package to ros 2
    
    Signed-off-by: Karsten Knese <karsten@openrobotics.org>
    
    Co-authored-by: Brett <ponsler@gmail.com>
    Co-authored-by: Marc-Antoine Testier <41569481+MarcTestier@users.noreply.github.com>
    Co-authored-by: Chris Lalancette <clalancette@gmail.com>

--- current/src/urg_time.c
+++ current/src/urg_time.c
@@ -20,8 +20,8 @@ void urg_walltime(unsigned long *sec, unsigned long *nsec)
 #else
     struct timeval timeofday;
     gettimeofday(&timeofday,NULL);
-    sec  = timeofday.tv_sec;
-    nsec = timeofday.tv_usec * 1000;
+    *sec  = (unsigned long)timeofday.tv_sec;
+    *nsec = timeofday.tv_usec * 1000;
 #endif
 #else
     // Win32 implementation
